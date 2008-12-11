b12cdcc6350e19e30e9528d63630ac17c902ff40
davvid <davvid@ba5bf794-d048-0410-8f42-e1fdefefffeb>
2008-12-11 11:16:22 +0000

    Add a tags module to hold the jsonpickle-specific dictionary keys
    This module will be used by the new encoded format.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>
    
    git-svn-id: https://jsonpickle.googlecode.com/svn/trunk@35 ba5bf794-d048-0410-8f42-e1fdefefffeb

---
+++ src/jsonpickle/tags.py
@@ -0,0 +1,9 @@
+## These are properties used for track object types, etc.
+Builtin = 'py/builtin'
+Object = 'py/object'
+Type = 'py/type'
+Repr = 'py/repr'
+Ref = 'py/ref'
+
+# All reserved tag names
+Reserved = Object, Type, Repr, Ref,
