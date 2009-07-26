a7cd17128d0b512e4bbd9a01c54d12645e77146d
davvid <davvid@ba5bf794-d048-0410-8f42-e1fdefefffeb>
2009-07-26 04:03:23 +0000

    setup.py: Add a main() method to make setup.py import-safe
    This setup.py import-safe by wrapping its setup() call
    in a main() method.
    
    Signed-off-by: Dan Buch <daniel.buch@gmail.com>
    Signed-off-by: David Aguilar <davvid@gmail.com>
    
    git-svn-id: https://jsonpickle.googlecode.com/svn/trunk@65 ba5bf794-d048-0410-8f42-e1fdefefffeb

--- src/setup.py
+++ src/setup.py
@@ -8,25 +8,34 @@
 # you should have received as part of this distribution.
 
 
+import sys
 import jsonpickle as _jsonpickle
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-    
-setup(
-    name = "jsonpickle",
-    version = _jsonpickle.__version__,
-    description = "Python library for serializing any arbitrary object graph into JSON",
+
+
+INSTALL_REQUIRES = []
+if sys.version_info[:2] <= (2, 5):
+    INSTALL_REQUIRES.append('simplejson')
+
+
+SETUP_ARGS = dict(
+    name="jsonpickle",
+    version=_jsonpickle.__version__,
+    description="Python library for serializing any "
+                "arbitrary object graph into JSON",
     long_description = _jsonpickle.__doc__,
-    author = "John Paulett",
-    author_email = "john@7oars.com",
-    url = "http://code.google.com/p/jsonpickle/",
-    license = "BSD",
-    platforms = ['POSIX', 'Windows'],
-    keywords = ['json pickle', 'json', 'pickle', 'marshal', 'serialization', 'JavaScript Object Notation'],
-    classifiers = [
+    author="John Paulett",
+    author_email="john@7oars.com",
+    url="http://code.google.com/p/jsonpickle/",
+    license="BSD",
+    platforms=['POSIX', 'Windows'],
+    keywords=['json pickle', 'json', 'pickle', 'marshal',
+              'serialization', 'JavaScript Object Notation'],
+    classifiers=[
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
@@ -35,12 +44,21 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: JavaScript"
     ],
-    options = { 'clean' : { 'all' : 1 } },
-    packages = ["jsonpickle"],
-    test_suite = 'jsonpickle.tests.suite',
-    install_requires=["simplejson"],
+    options={'clean': {'all': 1}},
+    packages=["jsonpickle"],
+    test_suite='jsonpickle.tests.suite',
+    install_requires=INSTALL_REQUIRES,
     extras_require = {
         "cjson":  ["python-cjson"]
     },
     zip_safe=True,
 )
+
+
+def main():
+    setup(**SETUP_ARGS)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
