7930765937f05470c7efc6fde50d3fc10d92f762
David Aguilar <davvid@gmail.com>
2013-02-08 00:45:21 -0800

    runtests.py: Fix sys.path handling
    Prepend into sys.path so that the source repo is preferred over other
    paths.  This avoids failing tests when jsonpickle is already installed
    in the default site-packages directory.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- tests/runtests.py
+++ tests/runtests.py
@@ -9,7 +9,8 @@
 
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(1, os.path.dirname(__file__))
+sys.path.insert(1, os.path.dirname(os.path.dirname(__file__)))
 
 import unittest
 
