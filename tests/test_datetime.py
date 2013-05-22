jsonpickle/jsonpickle

commit e27a48cd67133d2b7693a213ed82b79fac33c7f1

unpickler: simplify things by making _mkref() return its arguments
Chain more statements by making _mkref() into a pass-through function.

Signed-off-by: David Aguilar <davvid@gmail.com>

jsonpickle/unpickler.py
@@ -65,6 +65,7 @@ class Unpickler(object):
         if has_tag(obj, tags.ID):
             return self._pop(self._objs[obj[tags.ID]])
 
+        # Backwards compatibility
         if has_tag(obj, tags.REF):
             return self._pop(self._namedict.get(obj[tags.REF]))
 
@@ -74,25 +75,22 @@ class Unpickler(object):
                 return self._pop(obj)
             return self._pop(typeref)
 
+        # Backwards compatibility
         if has_tag(obj, tags.REPR):
             obj = loadrepr(obj[tags.REPR])
-            self._mkref(obj)
-            return self._pop(obj)
+            return self._pop(self._mkref(obj))
 
         if has_tag(obj, tags.OBJECT):
-
             cls = loadclass(obj[tags.OBJECT])
             if not cls:
-                self._mkref(obj)
-                return self._pop(obj)
+                return self._pop(self._mkref(obj))
 
             # check custom handlers
             HandlerClass = handlers.registry.get(cls)
             if HandlerClass:
                 handler = HandlerClass(self)
                 instance = handler.restore(obj)
-                self._mkref(instance)
-                return self._pop(instance)
+                return self._pop(self._mkref(instance))
 
             factory = loadfactory(obj)
             args = getargs(obj)
@@ -114,8 +112,7 @@ class Unpickler(object):
                     instance = cls()
                 except TypeError:
                     # fail gracefully if the constructor requires arguments
-                    self._mkref(obj)
-                    return self._pop(obj)
+                    return self._pop(self._mkref(obj))
 
             # Add to the instance table to allow being referenced by a
             # downstream object
@@ -156,17 +153,20 @@ class Unpickler(object):
             return self._pop([self.restore(v) for v in obj])
 
         if has_tag(obj, tags.TUPLE):
-            return self._pop(tuple([self.restore(v) for v in obj[tags.TUPLE]]))
+            return self._pop(tuple([self.restore(v)
+                                    for v in obj[tags.TUPLE]]))
 
         if has_tag(obj, tags.SET):
-            return self._pop(set([self.restore(v) for v in obj[tags.SET]]))
+            return self._pop(set([self.restore(v)
+                                  for v in obj[tags.SET]]))
 
         if util.is_dictionary(obj):
             data = {}
             for k, v in sorted(obj.items(), key=operator.itemgetter(0)):
                 self._namestack.append(k)
-                data[k] = self.restore(v)
+                data[k] = self._mkref(self.restore(v))
                 self._namestack.pop()
+
             return self._pop(data)
 
         return self._pop(obj)
@@ -210,11 +210,14 @@ class Unpickler(object):
         """
         obj_id = id(obj)
         try:
-            idx = self._obj_to_idx[obj_id]
+            self._obj_to_idx[obj_id]
         except KeyError:
-            idx = self._obj_to_idx[obj_id] = len(self._objs)
+            self._obj_to_idx[obj_id] = len(self._objs)
             self._objs.append(obj)
-        return idx
+            # Backwards compatibility: old versions of jsonpickle
+            # produced "py/ref" references.
+            self._namedict[self._refname()] = obj
+        return obj
 
 
 def loadclass(module_and_name):

commit ea3ac4830212fde7a22cae7bfde02cd0622229ba

unpickler: fix datetimes within objects
e27a48cd67133d2b7693a213ed82b79fac33c7f1 introduced
a regression by holding references to the outer dict
object, which is not needed.

Add a testcase for datetimes within objects and
fix the unpickler by removing the call to _mkref().

Closes #22

Signed-off-by: David Aguilar <davvid@gmail.com>

jsonpickle/unpickler.py
@@ -168,7 +168,7 @@ class Unpickler(object):
             data = {}
             for k, v in sorted(obj.items(), key=operator.itemgetter(0)):
                 self._namestack.append(k)
-                data[k] = self._mkref(self.restore(v))
+                data[k] = self.restore(v)
                 self._namestack.pop()
 
             return self._pop(data)

tests/samples.py
@@ -7,6 +7,7 @@
 # you should have received as part of this distribution.
 
 import collections
+import datetime
 from jsonpickle.compat import set
 
 
@@ -144,3 +145,10 @@ class Question(Node):
 
     def __repr__(self):
         return self.__str__()
+
+
+class ObjWithDate(object):
+    def __init__(self):
+        ts = datetime.datetime.now()
+        self.data = dict(a='a', ts=ts)
+        self.data_ref = dict(b='b', ts=ts)

tests/test_datetime.py
@@ -11,6 +11,9 @@ import datetime
 
 import jsonpickle
 
+from samples import ObjWithDate
+
+
 # UTC implementation from Python 2.7 docs
 class UTC(datetime.tzinfo):
     """UTC"""
@@ -77,6 +80,14 @@ class DateTimeTests(unittest.TestCase):
         pickler = jsonpickle.Pickler(unpicklable=False)
         flattened = pickler.flatten(obj)
         self.assertEqual(str(obj), flattened)
+
+    def test_object_with_datetime(self):
+        test_obj = ObjWithDate()
+        json = jsonpickle.encode(test_obj)
+        test_obj_decoded = jsonpickle.decode(json)
+        self.assertEqual(test_obj_decoded.data['ts'],
+                         test_obj_decoded.data_ref['ts'])
+
 
 
 def suite():