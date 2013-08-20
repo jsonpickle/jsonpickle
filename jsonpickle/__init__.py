e6634a3fbcdbf525a4832f33ac547573bc3a6650
David Aguilar <davvid@gmail.com>
2013-08-20 02:09:47 -0700

    administrativa: update copyright
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- jsonpickle/__init__.py
+++ jsonpickle/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2008 John Paulett (john -at- paulett.org)
-# Copyright (C) 2009-2013 David Aguilar (davvid -at- gmail)
+# Copyright (C) 2009, 2011, 2013 David Aguilar (davvid -at- gmail.com)
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
