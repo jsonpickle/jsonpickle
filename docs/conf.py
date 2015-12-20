5ad18328e8f38abcc5a0fc4a549e7a3c40697d4e
Jason R. Coombs <jaraco@jaraco.com>
2015-12-19 22:29:56 -0500

    Generate project skeleton

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools_scm
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+# General information about the project.
+project = 'skeleton'
+copyright = '2015 Jason R. Coombs'
+
+# The short X.Y version.
+version = setuptools_scm.get_version(root='..', relative_to=__file__)
+# The full version, including alpha/beta/rc tags.
+release = version
+
+master_doc = 'index'
