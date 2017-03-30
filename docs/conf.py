908cf4ad0e27813933ada7cc9f16ebce9ac0c6cc
Jason R. Coombs <jaraco@jaraco.com>
2017-03-30 04:36:53 -0400

    Rely on jaraco.packaging for loading the package metadata from the package for Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import subprocess
-import datetime
-
-if 'check_output' not in dir(subprocess):
-	import subprocess32 as subprocess
-
 extensions = [
     'sphinx.ext.autodoc',
+    'jaraco.packaging.sphinx',
     'rst.linker',
 ]
-
-# General information about the project.
-
-root = os.path.join(os.path.dirname(__file__), '..')
-setup_script = os.path.join(root, 'setup.py')
-fields = ['--name', '--version', '--url', '--author']
-dist_info_cmd = [sys.executable, setup_script] + fields
-output_bytes = subprocess.check_output(dist_info_cmd, cwd=root)
-project, version, url, author = output_bytes.decode('utf-8').strip().split('\n')
-
-copyright = author
-
-# The full version, including alpha/beta/rc tags.
-release = version
 
 master_doc = 'index'
 
@@ -34,13 +13,11 @@ link_files = {
 	'../CHANGES.rst': dict(
 		using=dict(
 			GH='https://github.com',
-			project=project,
-			url=url,
 		),
 		replace=[
 			dict(
 				pattern=r"(Issue )?#(?P<issue>\d+)",
-				url='{url}/issues/{issue}',
+				url='{package_url}/issues/{issue}',
 			),
 			dict(
 				pattern=r"^(?m)((?P<scm_version>v?\d+(\.\d+){1,2}))\n[-=]+\n",
