16d68a9fd19e6a726cc86fd1e3ec5f2d24788345
Jason R. Coombs <jaraco@jaraco.com>
2017-08-14 08:14:57 -0400

    Restore support for namespace package declaration, selected on a 'nspkg_technique' setting

--- setup.py
+++ setup.py
@@ -11,6 +11,11 @@ with io.open('README.rst', encoding='utf-8') as readme:
 
 name = 'skeleton'
 description = ''
+nspkg_technique = 'native'
+"""
+Does this package use "native" namespace packages or
+pkg_resources "managed" namespace packages?
+"""
 
 params = dict(
 	name=name,
@@ -22,6 +27,10 @@ params = dict(
 	url="https://github.com/jaraco/" + name,
 	packages=setuptools.find_packages(),
 	include_package_data=True,
+	namespace_packages=(
+		name.split('.')[:-1] if nspkg_technique == 'managed'
+		else []
+	),
 	python_requires='>=2.7',
 	install_requires=[
 	],
