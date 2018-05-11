3dc697348da36807d3b6f97dbca2bd1f1bf9bbf2
Jason R. Coombs <jaraco@jaraco.com>
2018-05-11 10:18:12 -0400

    Restore names imported into jsonpickle module. Ref #203.

--- jsonpickle/__init__.py
+++ jsonpickle/__init__.py
@@ -61,6 +61,14 @@ from .backend import json
 from .pickler import encode
 from .unpickler import decode
 
+# Export other names not in __all__
+from .backend import JSONBackend  # noqa: F401
+from .version import __version__  # noqa: F401
+from .handlers import register  # noqa: F401
+from .handlers import unregister  # noqa: F401
+from .pickler import Pickler  # noqa: F401
+from .unpickler import Unpickler  # noqa: F401
+
 __all__ = ('encode', 'decode')
 
 # register built-in handlers
