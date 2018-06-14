440adac3c3f91519a1ff47114774dbd1d5baf676
Jason R. Coombs <jaraco@jaraco.com>
2018-06-14 15:09:33 -0400

    Rely on declarative config to create long_description.

--- setup.py
+++ setup.py
@@ -2,12 +2,7 @@
 
 # Project skeleton maintained at https://github.com/jaraco/skeleton
 
-import io
-
 import setuptools
-
-with io.open('README.rst', encoding='utf-8') as readme:
-	long_description = readme.read()
 
 name = 'skeleton'
 description = ''
@@ -23,7 +18,6 @@ params = dict(
 	author="Jason R. Coombs",
 	author_email="jaraco@jaraco.com",
 	description=description or name,
-	long_description=long_description,
 	url="https://github.com/jaraco/" + name,
 	packages=setuptools.find_packages(),
 	include_package_data=True,
