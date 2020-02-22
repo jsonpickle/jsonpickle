39d0dabf5956a2e7480e890d5bea8072febccbcd
David Aguilar <davvid@gmail.com>
2020-02-22 13:42:32 -0800

    docs: update author details
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -186,8 +186,8 @@ htmlhelp_basename = 'jsonpickledoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'jsonpickle.tex', u'jsonpickle Documentation',
-   u'John Paulett', 'manual'),
+  ('index', 'jsonpickle.tex', 'jsonpickle Documentation',
+   'David Aguilar', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
