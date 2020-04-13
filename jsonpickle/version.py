a9dcee08b96ea3b7079b0b5d6e6992b5c778b183
David Aguilar <davvid@gmail.com>
2020-04-12 23:55:31 -0700

    jsonpickle: allow importing from the source tree
    Allow jsonpickle to import without any external dependencies.

--- jsonpickle/version.py
+++ jsonpickle/version.py
@@ -1,4 +1,16 @@
-import importlib_metadata as metadata
+try:
+    import importlib_metadata as metadata
+except ImportError:
+    metadata = None
 
 
-__version__ = metadata.version('jsonpickle')
+def _get_version():
+    try:
+        version = metadata.version('jsonpickle')
+    except (AttributeError, ImportError):
+        version = __default_version__
+    return version
+
+
+__default_version__ = '0.0.0-alpha'
+__version__ = _get_version()
