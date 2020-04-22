dd096fc9d2e439a3bfc96b000bdfa5cff26c2f3d
Mihir <mihir.xtha@gmail.com>
2020-04-22 11:58:53 -0400

    added a new test to check enums

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,4 +1,23 @@
 import jsonpickle
+from enum import Enum
+
+
+# Simple Enum Class
+class Type(Enum):
+    ONE = 1
+    TWO = 2
+
+
+class TestEnumObject:
+    def __init__(self, num, objectType):
+        self.num = num
+        self.type = objectType
+
+    def __repr__(self):
+        return "TestEnumObject(num={}, objectType={})".format(self.num, self.type)
+
+    def __str__(self):
+        return "TestEnumObject(num={}, objectType={})".format(self.num, self.type)
 
 
 # Dummy class to test @property issue
@@ -51,3 +70,25 @@ def test_dictionaryIdentity():
     # Testing it it works with the encoded list
     z[0]["name"] = "David"
     assert z[0] == z[1]
+
+
+def test_enumIssue():
+    a = TestEnumObject(1, Type.ONE)
+    b = TestEnumObject(2, Type.ONE)
+    c = TestEnumObject(3, Type.TWO)
+    d = TestEnumObject(1, Type.ONE)
+
+    # Different enum values, so they both should not equal each other
+    differentEnums = [a, c]
+    differentEnumsDecoded = jsonpickle.decode(jsonpickle.encode(differentEnums))
+    assert str(differentEnumsDecoded[0]) != str(differentEnumsDecoded[1])
+
+    # Same enum values, but different numbers, so they should still not equal each other
+    sameEnumsDifValues = [a, b]
+    sameEnumsDifValuesDecoded = jsonpickle.decode(jsonpickle.encode(sameEnumsDifValues))
+    assert str(sameEnumsDifValuesDecoded[0]) != str(sameEnumsDifValuesDecoded[1])
+
+    # Exact same enum and num values, so both should equal other other.
+    exactlySameEnums = [a, d]
+    exactlySameEnumsDecoded = jsonpickle.decode(jsonpickle.encode(exactlySameEnums))
+    assert str(exactlySameEnumsDecoded[0]) == str(exactlySameEnumsDecoded[1])
