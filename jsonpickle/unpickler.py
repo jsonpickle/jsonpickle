29a5584d80d0e0be1e612ab6771360acdf33242e
David Aguilar <davvid@gmail.com>
2024-04-27 13:33:54 -0700

    unpickler: use a generator when updating self._classes
    Related-to: #494

--- jsonpickle/unpickler.py
+++ jsonpickle/unpickler.py
@@ -390,10 +390,13 @@ class Unpickler(object):
             for cls in classes:
                 self.register_classes(cls)
         elif isinstance(classes, dict):
-            self._classes.update({
-                cls if isinstance(cls, str) else util.importable_name(cls): handler
+            self._classes.update(
+                (
+                    cls if isinstance(cls, str) else util.importable_name(cls),
+                    handler,
+                )
                 for cls, handler in classes.items()
-            })
+            )
         else:
             self._classes[util.importable_name(classes)] = classes
 
