9315e4bd3478b433aa7753d0f563aacc658eec3b
Theelx <Theelx@users.noreply.github.com>
2024-06-09 18:40:04 +0000

    Apply black to code

--- benchmarking/analyze_benchmarks.py
+++ benchmarking/analyze_benchmarks.py
@@ -23,7 +23,7 @@ def main():
         csv_reader = csv.reader(f)
         # skip header row
         next(csv_reader)
-        
+
         # csv is sorted by name so we can assume even rows are main and odd are new
         for indx, (func, median) in enumerate(csv_reader):
             func = func.split("::")[-1]
@@ -33,24 +33,31 @@ def main():
                 main.append(float(median))
             else:
                 new.append(float(median))
-        
+
     main = np.array(main)
     new = np.array(new)
-        
+
     relative_perfs = new / main
     # more stats will be included when needed
-    print(f"The new branch is {round(relative_perfs.mean(), 4)}x faster than the main branch aross all tests!")
+    print(
+        f"The new branch is {round(relative_perfs.mean(), 4)}x faster than the main branch aross all tests!"
+    )
 
     results = {}
 
     for func_name, relative_perf in zip(funcs, relative_perfs):
         # this allows us to categorize classes of results, such as encode vs decode
         results[func_name] = relative_perf
-    
+
     encode_perfs = filter_results_by_name(results, "encode")
     decode_perfs = filter_results_by_name(results, "decode")
-    print(f"The new branch is {round(encode_perfs.mean(), 4)}x faster than the main branch aross encode tests!")
-    print(f"The new branch is {round(decode_perfs.mean(), 4)}x faster than the main branch aross decode tests!")
+    print(
+        f"The new branch is {round(encode_perfs.mean(), 4)}x faster than the main branch aross encode tests!"
+    )
+    print(
+        f"The new branch is {round(decode_perfs.mean(), 4)}x faster than the main branch aross decode tests!"
+    )
+
 
 if __name__ == "__main__":
     main()
