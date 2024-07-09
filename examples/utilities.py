5f5cfb70ace06f304795e5643995637e1677812d
Theelx <43764914+Theelx@users.noreply.github.com>
2024-07-09 09:58:02 -0400

    Fix sys.exit error code and switch .txt to .json

--- examples/utilities.py
+++ examples/utilities.py
@@ -4,12 +4,9 @@ import sys
 
 def ensure_no_files_overwritten(expected_contents):
     # ensure we don't overwrite the user's files, if they run it on their machine
-    if os.path.exists(os.path.join(os.getcwd(), 'example.txt')):
-        with open("example.txt", "r") as f:
+    if os.path.exists(os.path.join(os.getcwd(), 'example.json')):
+        with open("example.json", "r") as f:
             contents = f.read()
         if contents != expected_contents:
-            print(
-                "I don't want to overwrite the example.txt file in my current directory! Please delete example.txt and try again!"
-            )
             # exit with a non-zero error code
-            sys.exit(1)
+            sys.exit("I don't want to overwrite the example.json file in my current directory! Please delete example.txt and try again!")
