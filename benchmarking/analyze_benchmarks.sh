50a5e090169a53c1c452d1b5dd697a327f66d07f
Theelx <43764914+Theelx@users.noreply.github.com>
2024-10-21 11:58:40 -0400

    Modify auxiliary benchmarking scripts to work with new Makefile

--- benchmarking/analyze_benchmarks.sh
+++ benchmarking/analyze_benchmarks.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 DATEANDTIME=$(date +%Y-%m-%dT%T%z)
+
+# assume user is running this cd'ed into this dir
+cd ..
+
 # we assume the user is running on Linux because I don't believe the benchmark setup works on MacOS or Windows anyway
-# user will need to have `schedutils` installed
-taskset -c 1 pytest --benchmark-disable-gc --benchmark-warmup=on --benchmark-warmup-iterations=1 --benchmark-min-rounds=10000 --benchmark-json=./benchmark-new.json --benchmark-histogram=../images/benchmark-$DATEANDTIME ../jsonpickle_benchmarks.py
+# user will need to be willing to install `util-linux` on Ubuntu
+make benchmark EXTRA_BENCH_ARGS="--benchmark-json=./benchmarking/benchmark-new.json"
 # assuming the user is doing work in a different branch than main
 git checkout main
-taskset -c 1 pytest --benchmark-disable-gc --benchmark-warmup=on --benchmark-warmup-iterations=1 --benchmark-min-rounds=10000 --benchmark-json=./benchmark-main.json --benchmark-histogram=../images/benchmark-$DATEANDTIME ../jsonpickle_benchmarks.py
+
+make benchmark EXTRA_BENCH_ARGS="--benchmark-json=./benchmarking/benchmark-main.json"
 # return to previous branch
 git checkout -
+
+cd benchmarking
+
 pytest-benchmark compare --columns=median --name=long --sort=fullname --csv=perf --histogram=perf benchmark-main.json benchmark-new.json
 python3 analyze_benchmarks.py
