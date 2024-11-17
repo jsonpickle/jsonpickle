09d69f1f2ffc625f11e675cb8308bf10b8bbd452
David Aguilar <davvid@gmail.com>
2024-11-17 00:35:10 -0800

    tests: update util_test.py to use the modern pytest style

--- tests/util_test.py
+++ tests/util_test.py
@@ -7,9 +7,8 @@
 
 import io
 import time
-import unittest
 
-from jsonpickle import compat, util
+from jsonpickle import util
 
 
 class Thing:
@@ -50,186 +49,209 @@ class MethodTestOldStyle:
         pass
 
 
-class UtilTestCase(unittest.TestCase):
-    def test_is_primitive_int(self):
-        self.assertTrue(util.is_primitive(0))
-        self.assertTrue(util.is_primitive(3))
-        self.assertTrue(util.is_primitive(-3))
+def test_is_primitive_int():
+    assert util.is_primitive(0)
+    assert util.is_primitive(3)
+    assert util.is_primitive(-3)
 
-    def test_is_primitive_float(self):
-        self.assertTrue(util.is_primitive(0))
-        self.assertTrue(util.is_primitive(3.5))
-        self.assertTrue(util.is_primitive(-3.5))
-        self.assertTrue(util.is_primitive(float(3)))
 
-    def test_is_primitive_long(self):
-        self.assertTrue(util.is_primitive(2**64))
+def test_is_primitive_float():
+    assert util.is_primitive(0)
+    assert util.is_primitive(3.5)
+    assert util.is_primitive(-3.5)
+    assert util.is_primitive(float(3))
 
-    def test_is_primitive_bool(self):
-        self.assertTrue(util.is_primitive(True))
-        self.assertTrue(util.is_primitive(False))
 
-    def test_is_primitive_None(self):
-        self.assertTrue(util.is_primitive(None))
+def test_is_primitive_long():
+    assert util.is_primitive(2**64)
 
-    def test_is_primitive_bytes(self):
-        self.assertFalse(util.is_primitive(b'hello'))
-        self.assertFalse(util.is_primitive(b'foo'))
-        self.assertTrue(util.is_primitive('foo'))
 
-    def test_is_primitive_unicode(self):
-        self.assertTrue(util.is_primitive(compat.ustr('')))
-        self.assertTrue(util.is_primitive('hello'))
+def test_is_primitive_bool():
+    assert util.is_primitive(True)
+    assert util.is_primitive(False)
 
-    def test_is_primitive_list(self):
-        self.assertFalse(util.is_primitive([]))
-        self.assertFalse(util.is_primitive([4, 4]))
 
-    def test_is_primitive_dict(self):
-        self.assertFalse(util.is_primitive({'key': 'value'}))
-        self.assertFalse(util.is_primitive({}))
+def test_is_primitive_None():
+    assert util.is_primitive(None)
 
-    def test_is_primitive_tuple(self):
-        self.assertFalse(util.is_primitive((1, 3)))
-        self.assertFalse(util.is_primitive((1,)))
 
-    def test_is_primitive_set(self):
-        self.assertFalse(util.is_primitive({1, 3}))
+def test_is_primitive_bytes():
+    assert not util.is_primitive(b'hello')
+    assert not util.is_primitive(b'foo')
+    assert util.is_primitive('foo')
 
-    def test_is_primitive_object(self):
-        self.assertFalse(util.is_primitive(Thing('test')))
 
-    def test_is_list_list(self):
-        self.assertTrue(util.is_list([1, 2]))
+def test_is_primitive_unicode():
+    assert util.is_primitive('')
+    assert util.is_primitive('hello')
 
-    def test_is_list_set(self):
-        self.assertTrue(util.is_set({1, 2}))
 
-    def test_is_list_tuple(self):
-        self.assertTrue(util.is_tuple((1, 2)))
+def test_is_primitive_list():
+    assert not util.is_primitive([])
+    assert not util.is_primitive([4, 4])
 
-    def test_is_list_dict(self):
-        self.assertFalse(util.is_list({'key': 'value'}))
-        self.assertFalse(util.is_set({'key': 'value'}))
-        self.assertFalse(util.is_tuple({'key': 'value'}))
 
-    def test_is_list_other(self):
-        self.assertFalse(util.is_list(1))
-        self.assertFalse(util.is_set(1))
-        self.assertFalse(util.is_tuple(1))
+def test_is_primitive_dict():
+    assert not util.is_primitive({'key': 'value'})
+    assert not util.is_primitive({})
 
-    def test_is_sequence_various(self):
-        self.assertTrue(util.is_sequence([]))
-        self.assertTrue(util.is_sequence(tuple()))
-        self.assertTrue(util.is_sequence(set()))
 
-    def test_is_dictionary_dict(self):
-        self.assertTrue(util.is_dictionary({}))
+def test_is_primitive_tuple():
+    assert not util.is_primitive((1, 3))
+    assert not util.is_primitive((1,))
 
-    def test_is_dicitonary_sequences(self):
-        self.assertFalse(util.is_dictionary([]))
-        self.assertFalse(util.is_dictionary(set()))
 
-    def test_is_dictionary_tuple(self):
-        self.assertFalse(util.is_dictionary(tuple()))
+def test_is_primitive_set():
+    assert not util.is_primitive({1, 3})
 
-    def test_is_dictionary_primitive(self):
-        self.assertFalse(util.is_dictionary(int()))
-        self.assertFalse(util.is_dictionary(None))
-        self.assertFalse(util.is_dictionary(''))
 
-    def test_is_dictionary_subclass_dict(self):
-        self.assertFalse(util.is_dictionary_subclass({}))
+def test_is_primitive_object():
+    assert not util.is_primitive(Thing('test'))
 
-    def test_is_dictionary_subclass_subclass(self):
-        self.assertTrue(util.is_dictionary_subclass(DictSubclass()))
 
-    def test_is_sequence_subclass_subclass(self):
-        self.assertTrue(util.is_sequence_subclass(ListSubclass()))
+def test_is_list_list():
+    assert util.is_list([1, 2])
 
-    def test_is_sequence_subclass_list(self):
-        self.assertFalse(util.is_sequence_subclass([]))
 
-    def test_is_noncomplex_time_struct(self):
-        t = time.struct_time('123456789')
-        self.assertTrue(util.is_noncomplex(t))
+def test_is_list_set():
+    assert util.is_set({1, 2})
 
-    def test_is_noncomplex_other(self):
-        self.assertFalse(util.is_noncomplex('a'))
 
-    def test_is_function_builtins(self):
-        self.assertTrue(util.is_function(globals))
+def test_is_list_tuple():
+    assert util.is_tuple((1, 2))
 
-    def test_is_function_lambda(self):
-        self.assertTrue(util.is_function(lambda: False))
 
-    def test_is_function_instance_method(self):
-        class Foo:
-            def method(self):
-                pass
+def test_is_list_dict():
+    assert not util.is_list({'key': 'value'})
+    assert not util.is_set({'key': 'value'})
+    assert not util.is_tuple({'key': 'value'})
 
-            @staticmethod
-            def staticmethod():
-                pass
 
-            @classmethod
-            def classmethod(cls):
-                pass
+def test_is_list_other():
+    assert not util.is_list(1)
+    assert not util.is_set(1)
+    assert not util.is_tuple(1)
 
-        f = Foo()
-        self.assertTrue(util.is_function(f.method))
-        self.assertTrue(util.is_function(f.staticmethod))
-        self.assertTrue(util.is_function(f.classmethod))
 
-    def test_itemgetter(self):
-        expect = '0'
-        actual = util.itemgetter((0, 'zero'))
-        self.assertEqual(expect, actual)
+def test_is_sequence_various():
+    assert util.is_sequence([])
+    assert util.is_sequence(tuple())
+    assert util.is_sequence(set())
 
-    def test_has_method(self):
-        instance = MethodTestClass()
-        x = 1
-        has_method = util.has_method
 
-        # no attribute
-        self.assertFalse(has_method(instance, 'foo'))
+def test_is_dictionary_dict():
+    assert util.is_dictionary({})
 
-        # builtin method type
-        self.assertFalse(has_method(int, '__getnewargs__'))
-        self.assertTrue(has_method(x, '__getnewargs__'))
 
-        # staticmethod
-        self.assertTrue(has_method(instance, 'static_method'))
-        self.assertTrue(has_method(MethodTestClass, 'static_method'))
+def test_is_dicitonary_sequences():
+    assert not util.is_dictionary([])
+    assert not util.is_dictionary(set())
 
-        # not a method
-        self.assertFalse(has_method(instance, 'variable'))
-        self.assertFalse(has_method(MethodTestClass, 'variable'))
 
-        # classmethod
-        self.assertTrue(has_method(instance, 'class_method'))
-        self.assertTrue(has_method(MethodTestClass, 'class_method'))
+def test_is_dictionary_tuple():
+    assert not util.is_dictionary(tuple())
 
-        # bound method
-        self.assertTrue(has_method(instance, 'bound_method'))
-        self.assertFalse(has_method(MethodTestClass, 'bound_method'))
 
-        # subclass bound method
-        sub_instance = MethodTestSubclass()
-        self.assertTrue(has_method(sub_instance, 'bound_method'))
-        self.assertFalse(has_method(MethodTestSubclass, 'bound_method'))
+def test_is_dictionary_primitive():
+    assert not util.is_dictionary(int())
+    assert not util.is_dictionary(None)
+    assert not util.is_dictionary('')
 
-        # old style object
-        old_instance = MethodTestOldStyle()
-        self.assertTrue(has_method(old_instance, 'bound_method'))
-        self.assertFalse(has_method(MethodTestOldStyle, 'bound_method'))
 
-    def test_importable_name(self):
-        func_being_tested_obj = util.importable_name
-        io_method_obj = io.BytesIO(b'bytes').readline
-        self.assertEqual(
-            util.importable_name(func_being_tested_obj),
-            'jsonpickle.util.importable_name',
-        )
-        self.assertEqual(util.importable_name(io_method_obj), '_io.BytesIO.readline')
-        self.assertEqual(util.importable_name(dict), 'builtins.dict')
+def test_is_dictionary_subclass_dict():
+    assert not util.is_dictionary_subclass({})
+
+
+def test_is_dictionary_subclass_subclass():
+    assert util.is_dictionary_subclass(DictSubclass())
+
+
+def test_is_sequence_subclass_subclass():
+    assert util.is_sequence_subclass(ListSubclass())
+
+
+def test_is_sequence_subclass_list():
+    assert not util.is_sequence_subclass([])
+
+
+def test_is_noncomplex_time_struct():
+    t = time.struct_time('123456789')
+    assert util.is_noncomplex(t)
+
+
+def test_is_noncomplex_other():
+    assert not util.is_noncomplex('a')
+
+
+def test_is_function_builtins():
+    assert util.is_function(globals)
+
+
+def test_is_function_lambda():
+    assert util.is_function(lambda: False)
+
+
+def test_is_function_instance_method():
+    class Foo:
+        def method(self):
+            pass
+
+        @staticmethod
+        def staticmethod():
+            pass
+
+        @classmethod
+        def classmethod(cls):
+            pass
+
+    f = Foo()
+    assert util.is_function(f.method)
+    assert util.is_function(f.staticmethod)
+    assert util.is_function(f.classmethod)
+
+
+def test_itemgetter():
+    expect = '0'
+    actual = util.itemgetter((0, 'zero'))
+    assert expect == actual
+
+
+def test_has_method():
+    instance = MethodTestClass()
+    x = 1
+    has_method = util.has_method
+    # no attribute
+    assert not has_method(instance, 'foo')
+    # builtin method type
+    assert not has_method(int, '__getnewargs__')
+    assert has_method(x, '__getnewargs__')
+    # staticmethod
+    assert has_method(instance, 'static_method')
+    assert has_method(MethodTestClass, 'static_method')
+    # not a method
+    assert not has_method(instance, 'variable')
+    assert not has_method(MethodTestClass, 'variable')
+    # classmethod
+    assert has_method(instance, 'class_method')
+    assert has_method(MethodTestClass, 'class_method')
+    # bound method
+    assert has_method(instance, 'bound_method')
+    assert not has_method(MethodTestClass, 'bound_method')
+    # subclass bound method
+    sub_instance = MethodTestSubclass()
+    assert has_method(sub_instance, 'bound_method')
+    assert not has_method(MethodTestSubclass, 'bound_method')
+    # old style object
+    old_instance = MethodTestOldStyle()
+    assert has_method(old_instance, 'bound_method')
+    assert not has_method(MethodTestOldStyle, 'bound_method')
+
+
+def test_importable_name():
+    func_being_tested_obj = util.importable_name
+    io_method_obj = io.BytesIO(b'bytes').readline
+    assert (
+        util.importable_name(func_being_tested_obj) == 'jsonpickle.util.importable_name'
+    )
+    assert util.importable_name(io_method_obj) == '_io.BytesIO.readline'
+    assert util.importable_name(dict) == 'builtins.dict'
