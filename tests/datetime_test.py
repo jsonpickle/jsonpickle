924414e71952606297d50b272b3ac50cfcaa3302
David Aguilar <davvid@gmail.com>
2024-11-17 00:19:03 -0800

    tests: update datetime_test.py to use the modern pytest style

--- tests/datetime_test.py
+++ tests/datetime_test.py
@@ -89,176 +89,153 @@ class PersistantVariables:
         return str(self._data)
 
 
-class DateTimeInnerReferenceTestCase(unittest.TestCase):
-    def test_object_with_inner_datetime_refs(self):
-        pvars = PersistantVariables()
-        pvars['z'] = 1
-        pvars['z2'] = 2
-        pickled = jsonpickle.encode(pvars)
-        obj = jsonpickle.decode(pickled)
-
-        # ensure the references are valid
-        self.assertTrue(obj['z']._dt_read is obj['z']._dt_write)
-        self.assertTrue(obj['z2']._dt_read is obj['z2']._dt_write)
-
-        # ensure the values are valid
-        self.assertEqual(obj['z'].get(), 1)
-        self.assertEqual(obj['z2'].get(), 2)
-
-        # ensure get() updates _dt_read
-        self.assertTrue(obj['z']._dt_read is not obj['z']._dt_write)
-        self.assertTrue(obj['z2']._dt_read is not obj['z2']._dt_write)
+def test_object_with_inner_datetime_refs():
+    pvars = PersistantVariables()
+    pvars['z'] = 1
+    pvars['z2'] = 2
+    pickled = jsonpickle.encode(pvars)
+    obj = jsonpickle.decode(pickled)
+    # ensure the references are valid
+    assert obj['z']._dt_read is obj['z']._dt_write
+    assert obj['z2']._dt_read is obj['z2']._dt_write
+    # ensure the values are valid
+    assert obj['z'].get() == 1
+    assert obj['z2'].get() == 2
+    # ensure get() updates _dt_read
+    assert obj['z']._dt_read is not obj['z']._dt_write
+    assert obj['z2']._dt_read is not obj['z2']._dt_write
 
 
-class DateTimeSimpleTestCase(unittest.TestCase):
-    def _roundtrip(self, obj):
-        """
-        pickle and then unpickle object, then assert the new object is the
-        same as the original.
-        """
-        pickled = jsonpickle.encode(obj)
-        unpickled = jsonpickle.decode(pickled)
-        self.assertEqual(obj, unpickled)
-
-    def test_datetime(self):
-        """
-        jsonpickle should pickle a datetime object
-        """
-        self._roundtrip(datetime.datetime.now())
-
-    def test_date(self):
-        """
-        jsonpickle should pickle a date object
-        """
-        self._roundtrip(datetime.datetime.today())
-
-    def test_time(self):
-        """
-        jsonpickle should pickle a time object
-        """
-        self._roundtrip(datetime.datetime.now().time())
-
-    def test_timedelta(self):
-        """
-        jsonpickle should pickle a timedelta object
-        """
-        self._roundtrip(datetime.timedelta(days=3))
-
-    def test_utc(self):
-        """
-        jsonpickle should be able to encode and decode a datetime with a
-        simple, pickleable UTC tzinfo.
-        """
-        self._roundtrip(
-            datetime.datetime.now(tz=datetime.timezone.utc).replace(tzinfo=utc)
-        )
-
-    def test_unpickleable(self):
-        """
-        If 'unpickleable' is set on the Pickler, the date objects should be
-        simple, human-readable strings.
-        """
-        obj = datetime.datetime.now()
-        pickler = jsonpickle.pickler.Pickler(unpicklable=False)
-        flattened = pickler.flatten(obj)
-        self.assertEqual(obj.isoformat(), flattened)
-
-    def test_object_with_datetime(self):
-        test_obj = ObjWithDate()
-        json = jsonpickle.encode(test_obj)
-        test_obj_decoded = jsonpickle.decode(json)
-        self.assertEqual(test_obj_decoded.data['ts'], test_obj_decoded.data_ref['ts'])
-
-    @unittest.skipIf(sys.version_info < (3, 9), "only tested for python >= 3.9")
-    def test_datetime_with_ZoneInfo(self):
-        """
-        jsonpickle should pickle a datetime object with time zone info
-        """
-        # if sys.version_info >= (3, 9):
-        from zoneinfo import ZoneInfo
-
-        now = datetime.datetime.now()
-
-        SaoPaulo = ZoneInfo('America/Sao_Paulo')
-        NewYork = ZoneInfo('America/New_York')
-
-        now_sp = now.replace(tzinfo=SaoPaulo)
-        now_us = now.replace(tzinfo=NewYork)
-
-        self._roundtrip(now_sp)
-        self._roundtrip(now_us)
+def _roundtrip(obj):
+    """Roundtrip encode and decode an object and assert equality"""
+    pickled = jsonpickle.encode(obj)
+    unpickled = jsonpickle.decode(pickled)
+    assert obj == unpickled
 
 
-class DateTimeAdvancedTestCase(unittest.TestCase):
-    def setUp(self):
-        self.pickler = jsonpickle.pickler.Pickler()
-        self.unpickler = jsonpickle.unpickler.Unpickler()
+def test_datetime():
+    """Roundtrip datetime objects"""
+    _roundtrip(datetime.datetime.now())
 
-    def tearDown(self):
-        self.pickler.reset()
-        self.unpickler.reset()
 
-    def test_struct_time(self):
-        expect = time.struct_time([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        json = jsonpickle.encode(expect)
-        actual = jsonpickle.decode(json)
-        self.assertEqual(type(actual), time.struct_time)
-        self.assertEqual(expect, actual)
+def test_date():
+    """Roundtrip date objects"""
+    _roundtrip(datetime.datetime.today())
 
-    def test_struct_time_chars(self):
-        expect = time.struct_time('123456789')
 
-        flattened = self.pickler.flatten(expect)
-        actual = self.unpickler.restore(flattened)
-        self.assertEqual(expect, actual)
+def test_time():
+    """Roundtrip time objects"""
+    _roundtrip(datetime.datetime.now().time())
 
-    def test_datetime_structure(self):
-        obj = datetime.datetime.now()
 
-        flattened = self.pickler.flatten(obj)
-        self.assertTrue(tags.OBJECT in flattened)
-        self.assertTrue('__reduce__' in flattened)
+def test_timedelta():
+    """Roundtrip timedelta objects"""
+    _roundtrip(datetime.timedelta(days=3))
 
-        inflated = self.unpickler.restore(flattened)
-        self.assertEqual(obj, inflated)
 
-    def test_datetime_inside_int_keys_defaults(self):
-        t = datetime.time(hour=10)
-        s = jsonpickle.encode({1: t, 2: t})
-        d = jsonpickle.decode(s)
-        self.assertEqual(d["1"], d["2"])
-        self.assertTrue(d["1"] is d["2"])
-        self.assertTrue(isinstance(d["1"], datetime.time))
+def test_utc():
+    """Roundtrip datetime objectcs with UTC timezone info"""
+    _roundtrip(datetime.datetime.now(tz=datetime.timezone.utc).replace(tzinfo=utc))
 
-    def test_datetime_inside_int_keys_with_keys_enabled(self):
-        t = datetime.time(hour=10)
-        s = jsonpickle.encode({1: t, 2: t}, keys=True)
-        d = jsonpickle.decode(s, keys=True)
-        self.assertEqual(d[1], d[2])
-        self.assertTrue(d[1] is d[2])
-        self.assertTrue(isinstance(d[1], datetime.time))
 
-    def test_datetime_repr_not_unpicklable(self):
-        obj = datetime.datetime.now()
-        pickler = jsonpickle.pickler.Pickler(unpicklable=False)
-        flattened = pickler.flatten(obj)
-        assert tags.REPR not in flattened
-        assert tags.MODULE not in flattened
-        assert tags.OBJECT not in flattened
-        assert obj.isoformat() == flattened
+def test_unpickleable():
+    """Date objects are human-readable strings when unpicklable is False"""
+    obj = datetime.datetime.now()
+    pickler = jsonpickle.pickler.Pickler(unpicklable=False)
+    flattened = pickler.flatten(obj)
+    assert obj.isoformat() == flattened
 
-    def test_datetime_dict_keys_defaults(self):
-        """Test that we handle datetime objects as keys."""
-        datetime_dict = {datetime.datetime(2008, 12, 31): True}
-        pickled = jsonpickle.encode(datetime_dict)
-        expect = {'datetime.datetime(2008, 12, 31, 0, 0)': True}
-        actual = jsonpickle.decode(pickled)
-        self.assertEqual(expect, actual)
 
-    def test_datetime_dict_keys_with_keys_enabled(self):
-        """Test that we handle datetime objects as keys."""
-        datetime_dict = {datetime.datetime(2008, 12, 31): True}
-        pickled = jsonpickle.encode(datetime_dict, keys=True)
-        expect = datetime_dict
-        actual = jsonpickle.decode(pickled, keys=True)
-        self.assertEqual(expect, actual)
+def test_object_with_datetime():
+    test_obj = ObjWithDate()
+    json = jsonpickle.encode(test_obj)
+    test_obj_decoded = jsonpickle.decode(json)
+    assert test_obj_decoded.data['ts'] == test_obj_decoded.data_ref['ts']
+
+
+@unittest.skipIf(sys.version_info < (3, 9), 'only tested for python >= 3.9')
+def test_datetime_with_zoneinfo():
+    """Roundtrip datetime objects with ZoneInfo tzinfo"""
+    from zoneinfo import ZoneInfo
+
+    now = datetime.datetime.now()
+    SaoPaulo = ZoneInfo('America/Sao_Paulo')
+    NewYork = ZoneInfo('America/New_York')
+    now_sp = now.replace(tzinfo=SaoPaulo)
+    now_us = now.replace(tzinfo=NewYork)
+    _roundtrip(now_sp)
+    _roundtrip(now_us)
+
+
+def test_struct_time():
+    expect = time.struct_time([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    json = jsonpickle.encode(expect)
+    actual = jsonpickle.decode(json)
+    assert type(actual) is time.struct_time
+    assert expect == actual
+
+
+def test_struct_time_chars():
+    pickler = jsonpickle.pickler.Pickler()
+    unpickler = jsonpickle.unpickler.Unpickler()
+    expect = time.struct_time('123456789')
+    flattened = pickler.flatten(expect)
+    actual = unpickler.restore(flattened)
+    assert expect == actual
+
+
+def test_datetime_structure():
+    pickler = jsonpickle.pickler.Pickler()
+    unpickler = jsonpickle.unpickler.Unpickler()
+    obj = datetime.datetime.now()
+    flattened = pickler.flatten(obj)
+    assert tags.OBJECT in flattened
+    assert '__reduce__' in flattened
+    inflated = unpickler.restore(flattened)
+    assert obj == inflated
+
+
+def test_datetime_inside_int_keys_defaults():
+    t = datetime.time(hour=10)
+    s = jsonpickle.encode({1: t, 2: t})
+    d = jsonpickle.decode(s)
+    assert d['1'] == d['2']
+    assert d['1'] is d['2']
+    assert isinstance(d['1'], datetime.time)
+
+
+def test_datetime_inside_int_keys_with_keys_enabled():
+    t = datetime.time(hour=10)
+    s = jsonpickle.encode({1: t, 2: t}, keys=True)
+    d = jsonpickle.decode(s, keys=True)
+    assert d[1] == d[2]
+    assert d[1] is d[2]
+    assert isinstance(d[1], datetime.time)
+
+
+def test_datetime_repr_not_unpicklable():
+    obj = datetime.datetime.now()
+    pickler = jsonpickle.pickler.Pickler(unpicklable=False)
+    flattened = pickler.flatten(obj)
+    assert tags.REPR not in flattened
+    assert tags.MODULE not in flattened
+    assert tags.OBJECT not in flattened
+    assert obj.isoformat() == flattened
+
+
+def test_datetime_dict_keys_defaults():
+    """Test that we handle datetime objects as keys."""
+    datetime_dict = {datetime.datetime(2008, 12, 31): True}
+    pickled = jsonpickle.encode(datetime_dict)
+    expect = {'datetime.datetime(2008, 12, 31, 0, 0)': True}
+    actual = jsonpickle.decode(pickled)
+    assert expect == actual
+
+
+def test_datetime_dict_keys_with_keys_enabled():
+    """Test that we handle datetime objects as keys."""
+    datetime_dict = {datetime.datetime(2008, 12, 31): True}
+    pickled = jsonpickle.encode(datetime_dict, keys=True)
+    expect = datetime_dict
+    actual = jsonpickle.decode(pickled, keys=True)
+    assert expect == actual
