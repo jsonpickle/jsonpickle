d55aaf56cf2c347a9c155d991d346db87ab2fb70
David Aguilar <davvid@gmail.com>
2024-11-17 00:20:13 -0800

    tests: update document_test.py to use the modern pytest style

--- tests/document_test.py
+++ tests/document_test.py
@@ -1,5 +1,3 @@
-import unittest
-
 import jsonpickle
 
 
@@ -55,34 +53,30 @@ class Section(Node):
         return self.__str__()
 
 
-class DocumentTestCase(unittest.TestCase):
-    def test_cyclical(self):
-        """Test that we can pickle cyclical data structure
+def test_cyclical():
+    """Test that we can pickle cyclical data structure
 
-        This test is ensures that we can reference objects which
-        first appear within a list (in other words, not a top-level
-        object or attribute).  Later children will reference that
-        object through its "_parent" field.
+    This test is ensures that we can reference objects which
+    first appear within a list (in other words, not a top-level
+    object or attribute).  Later children will reference that
+    object through its "_parent" field.
 
-        This makes sure that we handle this case correctly.
+    This makes sure that we handle this case correctly.
 
-        """
-        document = Document('My Document')
-        section1 = Section('Section 1')
-        section2 = Section('Section 2')
-        question1 = Question('Question 1')
-        question2 = Question('Question 2')
-        question3 = Question('Question 3')
-        question4 = Question('Question 4')
-
-        document.add_child(section1)
-        document.add_child(section2)
-        section1.add_child(question1)
-        section1.add_child(question2)
-        section2.add_child(question3)
-        section2.add_child(question4)
-
-        pickled = jsonpickle.encode(document)
-        unpickled = jsonpickle.decode(pickled)
-
-        self.assertEqual(str(document), str(unpickled))
+    """
+    document = Document('My Document')
+    section1 = Section('Section 1')
+    section2 = Section('Section 2')
+    question1 = Question('Question 1')
+    question2 = Question('Question 2')
+    question3 = Question('Question 3')
+    question4 = Question('Question 4')
+    document.add_child(section1)
+    document.add_child(section2)
+    section1.add_child(question1)
+    section1.add_child(question2)
+    section2.add_child(question3)
+    section2.add_child(question4)
+    pickled = jsonpickle.encode(document)
+    unpickled = jsonpickle.decode(pickled)
+    assert str(document) == str(unpickled)
