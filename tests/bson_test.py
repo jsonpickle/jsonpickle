e1693ad687c8dc57ed38c252860e117ef7d8070a
David Aguilar <davvid@gmail.com>
2025-12-07 20:20:06 -0800

    tests: modernize the bson tests to leverage pytest
    Use a single pytest.skip() instead of repeating the check in every test.
    
    Related-to: #503

--- tests/bson_test.py
+++ tests/bson_test.py
@@ -4,11 +4,15 @@ import datetime
 import json
 import pickle
 
-from helper import SkippableTest
+import pytest
+
+try:
+    import bson
+    import bson.tz_util
+except ImportError:
+    pytest.skip('bson is not available', allow_module_level=True)
 
 import jsonpickle
-
-bson = None
 
 
 class Object:
@@ -19,65 +23,49 @@ class Object:
         return (self.offset,)
 
 
-class BSONTestCase(SkippableTest):
-    def setUp(self):
-        global bson
-        try:
-            bson = __import__('bson.tz_util')
-            self.should_skip = False
-        except ImportError:
-            self.should_skip = True
+def test_FixedOffsetSerializable():
+    fo = bson.tz_util.FixedOffset(-60 * 5, 'EST')
+    serialized = jsonpickle.dumps(fo)
+    restored = jsonpickle.loads(serialized)
+    assert vars(restored) == vars(fo)
 
-    def test_FixedOffsetSerializable(self):
-        if self.should_skip:
-            return self.skip('bson is not installed')
-        fo = bson.tz_util.FixedOffset(-60 * 5, 'EST')
-        serialized = jsonpickle.dumps(fo)
-        restored = jsonpickle.loads(serialized)
-        assert vars(restored) == vars(fo)
 
-    def test_timedelta(self):
-        if self.should_skip:
-            return self.skip('bson is not installed')
-        td = datetime.timedelta(-1, 68400)
-        serialized = jsonpickle.dumps(td)
-        restored = jsonpickle.loads(serialized)
-        assert restored == td
+def test_timedelta():
+    td = datetime.timedelta(-1, 68400)
+    serialized = jsonpickle.dumps(td)
+    restored = jsonpickle.loads(serialized)
+    assert restored == td
 
-    def test_stdlib_pickle(self):
-        if self.should_skip:
-            return self.skip('bson is not installed')
-        fo = bson.tz_util.FixedOffset(-60 * 5, 'EST')
-        serialized = pickle.dumps(fo)
-        restored = pickle.loads(serialized)
-        assert vars(restored) == vars(fo)
 
-    def test_nested_objects(self):
-        if self.should_skip:
-            return self.skip('bson is not installed')
-        o = Object(99)
-        serialized = jsonpickle.dumps(o)
-        restored = jsonpickle.loads(serialized)
-        assert restored.offset == datetime.timedelta(99)
+def test_stdlib_pickle():
+    fo = bson.tz_util.FixedOffset(-60 * 5, 'EST')
+    serialized = pickle.dumps(fo)
+    restored = pickle.loads(serialized)
+    assert vars(restored) == vars(fo)
 
-    def test_datetime_with_fixed_offset(self):
-        if self.should_skip:
-            return self.skip('bson is not installed')
-        fo = bson.tz_util.FixedOffset(-60 * 5, 'EST')
-        dt = datetime.datetime.now().replace(tzinfo=fo)
-        serialized = jsonpickle.dumps(dt)
-        restored = jsonpickle.loads(serialized)
-        assert restored == dt
 
-    def test_datetime_with_fixed_offset_incremental(self):
-        """Test creating an Unpickler and incrementally encoding"""
-        if self.should_skip:
-            return self.skip('bson is not installed')
-        obj = datetime.datetime(2019, 1, 29, 18, 9, 8, 826000, tzinfo=bson.tz_util.utc)
-        doc = jsonpickle.dumps(obj)
-        # Restore the json using a custom unpickler context.
-        unpickler = jsonpickle.unpickler.Unpickler()
-        jsonpickle.loads(doc, context=unpickler)
-        # Incrementally restore using the same context
-        clone = json.loads(doc, object_hook=lambda x: unpickler.restore(x, reset=False))
-        assert obj.tzinfo.__reduce__() == clone.tzinfo.__reduce__()
+def test_nested_objects():
+    o = Object(99)
+    serialized = jsonpickle.dumps(o)
+    restored = jsonpickle.loads(serialized)
+    assert restored.offset == datetime.timedelta(99)
+
+
+def test_datetime_with_fixed_offset():
+    fo = bson.tz_util.FixedOffset(-60 * 5, 'EST')
+    dt = datetime.datetime.now().replace(tzinfo=fo)
+    serialized = jsonpickle.dumps(dt)
+    restored = jsonpickle.loads(serialized)
+    assert restored == dt
+
+
+def test_datetime_with_fixed_offset_incremental():
+    """Test creating an Unpickler and incrementally encoding"""
+    obj = datetime.datetime(2019, 1, 29, 18, 9, 8, 826000, tzinfo=bson.tz_util.utc)
+    doc = jsonpickle.dumps(obj)
+    # Restore the json using a custom unpickler context.
+    unpickler = jsonpickle.unpickler.Unpickler()
+    jsonpickle.loads(doc, context=unpickler)
+    # Incrementally restore using the same context
+    clone = json.loads(doc, object_hook=lambda x: unpickler.restore(x, reset=False))
+    assert obj.tzinfo.__reduce__() == clone.tzinfo.__reduce__()
